35ccef0797805de87751c4547e6744b6b2a6a7ac
Alex Jenter <AlexJenter@users.noreply.github.com>
2025-09-19 11:22:22 +0200

    initial commit from alex

---
+++ src/content.config.ts
@@ -0,0 +1,17 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob, file } from 'astro/loaders';
+
+// 3. Define your collection(s)
+const fonts = defineCollection({
+    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/pages/fonts" }),
+    schema: z.object({
+        title: z.string(),
+        navSort: z.number()
+    })
+})
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { fonts };
